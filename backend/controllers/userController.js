50c65e5db337eac65fc3a370ad8157b4e3e76ff6
David Cox <davidcox@OnePlus_6T.thejoyfactory.com>
2023-09-28 13:20:44 -0700

    authentication API is working ready for front end

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,8 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
+const User = require('../models/userModel')
 
 //@desc Register a new user
 //@route /api/users
@@ -13,7 +16,36 @@ const registerUser = asyncHandler(async(req, res) => {
     res.status(400)
     throw new Error('Please include All Fields')
 }
-    res.send('Register Route')
+
+   //Does user already exist?
+   const userExists = await User.findOne({email})
+   if(userExists) {
+       res.status(400)
+       throw new Error('User already exists')
+   }
+
+   //Hash password
+   const salt = await bcrypt.genSalt(10)
+   const hashPassword = await bcrypt.hash(password, salt)
+
+   const user = await User.create({
+       name,
+       email,
+       password: hashPassword
+   })
+
+   if(user) {
+       res.status(201).json({
+           _id: user._id,
+           name: user.name,
+           email: user.email,
+           token: generateToken(user._id),
+       })
+   } else {
+       res.status(400)
+       throw new error('Invalid user data')
+   }
+
 })
 
 
@@ -22,10 +54,46 @@ const registerUser = asyncHandler(async(req, res) => {
 //@route /api/users/login
 //@access Public
 const loginUser = asyncHandler(async(req, res) => {
-    res.send('Login Route')
+    const {email, password} = req.body
+    const user = await User.findOne({email})
+
+    //Check if User and passwords match
+    if(user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user._id,
+           name: user.name,
+           email: user.email,
+           token: generateToken(user._id),
+        })
+    } else {
+        res.status(401)
+        throw new Error('Invalid credentials')
+    }
+ 
 })
+
+//@desc Get a current user
+//@route /api/users/mydash
+//@access Private
+const getDash = asyncHandler(async(req, res) => {
+    const user = {
+        id: req.user._id,
+        email: req.user.email,
+        name: req.user.name,
+    }
+
+    res.status(200).json(user)
+})
+
+//Generate a Token
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '2d',
+    })
+}
 
 module.exports = {
     registerUser,
     loginUser,
+    getDash,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { registerUser, loginUser } = require('../controllers/userController')
+const { registerUser, loginUser, getDash } = require('../controllers/userController')
+const { protect } = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
-
 router.post('/login', loginUser)
-
+router.get('/mydash', protect, getDash)
 
 module.exports = router
