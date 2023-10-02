18217a83ae654503cafbbc74b02028dc7a326498
David Cox <davidcox@OnePlus_6T.thejoyfactory.com>
2023-10-02 14:24:31 -0700

    working on registration form

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import { FaImages, FaQuestionCircle } from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+
+function Home() {
+    return (
+        <>
+            <section className="heading">
+                <h1>Access Your Partner Portal</h1>
+                <p>Choose from options below</p>
+            </section>
+
+            <Link to='/request-assets' className='btn btn-reverse btn-block'>
+                <FaQuestionCircle /> Request New Assets
+            </Link>
+            <Link to='/dashboard' className='btn btn-block'>
+                <FaImages /> View My Assets
+            </Link>
+        </>
+    )
+}
+
+export default Home
